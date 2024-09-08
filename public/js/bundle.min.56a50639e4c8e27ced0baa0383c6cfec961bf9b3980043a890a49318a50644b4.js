0c26ffd094e2dd21737c47a875ebf7d16ff56f27
eens <eens@localhost.my.domain>
2024-09-08 13:40:49 +0200

    update

--- public/js/bundle.min.56a50639e4c8e27ced0baa0383c6cfec961bf9b3980043a890a49318a50644b4.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".hamburger"),t=document.querySelector(".nav-links");e.addEventListener("click",function(){t.classList.toggle("active")})})
